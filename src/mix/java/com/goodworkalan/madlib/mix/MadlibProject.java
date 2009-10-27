abe633adfd640e9f8d52dc51bb4c119ca0b49a89
Alan Gutierrez <alan@blogometer.com>
2009-10-27 16:58:20 -0500

    Updated Mix build specification.

---
+++ src/mix/java/com/goodworkalan/madlib/mix/MadlibProject.java
@@ -0,0 +1,22 @@
+package com.goodworkalan.madlib.mix;
+
+import com.goodworkalan.go.go.Artifact;
+import com.goodworkalan.mix.ProjectModule;
+import com.goodworkalan.mix.builder.Builder;
+import com.goodworkalan.mix.builder.JavaProject;
+
+public class MadlibProject extends ProjectModule {
+    @Override
+    public void build(Builder builder) {
+        builder
+            .cookbook(JavaProject.class)
+                .produces(new Artifact("com.goodworkalan/madlib/0.1"))
+                .test()
+                    .depends()
+                        .artifact(new Artifact("org.testng/testng/5.10"))
+                        .end()
+                    .end()
+                .end()
+            .end();
+    }
+}
