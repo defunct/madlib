020c94b4e9966adc529491312c932d1e60f25be4
Alan Gutierrez <alan@blogometer.com>
2010-05-16 23:31:15 -0500

    Mix ProjectModule is now an interface.

--- src/mix/java/com/goodworkalan/madlib/mix/MadlibProject.java
+++ src/mix/java/com/goodworkalan/madlib/mix/MadlibProject.java
@@ -4,17 +4,25 @@ import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
 
-public class MadlibProject extends ProjectModule {
-    @Override
+/**
+ * Builds the project definition for Madlib.
+ *
+ * @author Alan Gutierrez
+ */
+public class MadlibProject implements ProjectModule {
+    /**
+     * Build the project definition for Madlib.
+     *
+     * @param builder
+     *          The project builder.
+     */
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
                 .produces("com.github.bigeasy.madlib/madlib/0.1.0.1")
-                .test()
-                    .depends()
-                        .include("org.testng/testng-jdk15/5.10")
-                        .include("org.mockito/mockito-core/1.6")
-                        .end()
+                .depends()
+                    .production("org.testng/testng-jdk15/5.10")
+                    .production("org.mockito/mockito-core/1.6")
                     .end()
                 .end()
             .end();
